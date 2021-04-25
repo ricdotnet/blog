7dbf06f58430eefab1cc832758fb014f90ec80b7
Ricardo Rocha <me@rrocha.uk>
2021-04-25 16:47:42 +0100

    hide db details;

--- functions/mysql.php
+++ functions/mysql.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$database = "databasename";
+$username = "ricdotnet";
+$password = "computerapplepc";
+$database = "blog";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
